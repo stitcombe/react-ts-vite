c464d52aa9fdc1523da0efd2eb68676dd801bea4
Stephen Titcombe <stitcombe@gmail.com>
2024-01-23 20:52:30 -0600

    refactor: misc updates

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,16 +1,15 @@
 /* eslint-disable */
 
-import { rest } from 'msw';
+import { DefaultBodyType, http, HttpResponse } from 'msw';
 
 const handlers = [
-  rest.post('https://abc.com/api/', async (_req, res, ctx) => {
+  http.post('https://abc.com/api/', async () => {
     // Simulate a 2-second delay before responding
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-
-    return res(
-      ctx.status(200),
-      ctx.json({ message: 'Record saved successfully' })
+    const newPost: DefaultBodyType = await new Promise((resolve) =>
+      setTimeout(resolve, 2000)
     );
+
+    return HttpResponse.json(newPost, { status: 201 });
   }),
 ];
 
